782f8fc8575daba588f91e528f8a5bb4e3cb94f6
YugBandhara <ybandhara@gmail.com>
2025-02-21 14:39:45 -0500

    Implemneted Backend and Frontend Connection

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ dotenv.config();
 
 
 const app = express();
+app.use(cors());
 app.use(express.json()); 
 const port = process.env.PORT || 4000;
 
@@ -24,13 +25,14 @@ connectDatabse()
     app.use("/story", indexRoute);
 
     // Middleware
-    app.use(express.json());
-    app.use(cors());
+    // app.use(express.json());
+    // app.use(cors());
+      
     app.use(cookieParser());
 
     app.get("/", (req, res) => {
       res.send("Hello, world!");
-    });
+    }); 
 
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
